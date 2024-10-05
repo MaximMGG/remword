8d80d5b96015ea12231ef64c5cc9499b8cc5cb61
MaximMGG <maxhrunenko@gmail.com>
2024-10-05 21:47:45 +0300

    need to rewirte with json parser lib

--- src/config_wr.c
+++ src/config_wr.c
@@ -1,6 +1,7 @@
 #include "../headers/config_wr.h"
 #include <stdlib.h>
 #include <cstdext/estring.h>
+#include <cstdext/jsonparser.h>
 #include <unistd.h>
 #include <stdio.h>
 #include <dirent.h>
@@ -15,7 +16,6 @@
                             "--file:\n"
 
 
-
 Config_main *Config_read_config() {
     Config_main *conf = new(Config_main);
     conf->config_folder = null;
@@ -24,30 +24,9 @@ Config_main *Config_read_config() {
     str path_to_conf = news(256);
     snprintf(path_to_conf, 256, STD_CONFIG_PATH, conf->user_name);
 
-    FILE *conf_f = fopen(path_to_conf, "r");
-    if (conf_f == null) {
-        char buf[256] = {0};
-        snprintf(buf, 256, STD_PATH_TO_DIR, conf->user_name);
-        mkdir(buf, 0700);
-        conf_f = fopen(path_to_conf, "w");
-        if (conf_f == null) {
-            REM_ERROR("Cant create file with path %s\n", path_to_conf);
-            return null;
-        }
-        fwrite(STD_CONFIG_CONTENT, 1, strlen(STD_CONFIG_CONTENT), conf_f);
-        fclose(conf_f);
-        return conf;
-    }
+    JSON_OBJ *config = jsonCreateFromFile(path_to_conf);
+    
 
-    arraylist *conf_cont = arraylistFromFile(path_to_conf);
-    for(i32 i = 0; i < conf_cont->len; i++) {
-        i32 find = strFind(arraylistGet(conf_cont, i), "working_dir:", 0);
-        if (find != -1) {
-            //TODO (maxim) implemet after strSubString will be written
-            // str working_dir = strSubString();
-        }
-
-    }
 
     return null;
 }
