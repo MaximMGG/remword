795b2df4e53ea31cb697c509f3b318b2b3a833da
MaximMGG <maxhrunenko@gmail.com>
2024-10-15 18:35:51 +0300

    menues

--- src/config_wr.c
+++ src/config_wr.c
@@ -45,11 +45,72 @@ REM_CODE Config_add_dir(Config_main *config, str dir_name);
 REM_CODE Config_add_file(Config_main *config, str file_name, str dir_index);
 REM_CODE Config_write_config(Config_main *config);
 
+void print_file(arraylist *file_content) {
+        for(i32 i = 0; i < file_content->len; i++) {
+            printf("%d - %s\n", i + 1, (str) arraylistGet(file_content, i));
+        }
+}
 
 void print_file_cont_menu(Config_main *conf) {
+    arraylist *file_content = arraylistFromFile(conf->current_file);
+    printf( "1. Show words\n"
+            "2. Add word\n"
+            "3. Remove word\n"
+            "4. Start learning\n"
+            "5. Back to dir menu\n");
+
+    i32 user_select = 0;
+    fscanf(stdin, "%d", &user_select);
+    switch(user_select) {
+        case 1: {
+            if (file_content != null) {
+                print_file(file_content);
+                print_file_cont_menu(conf);
+            } else {
+                printf("Cant open file with name %s\n", conf->current_file);
+                print_dir_cont_menu(conf);
+            }
+        } break;
+        case 2: {
+            //addword(conf, file_content);
+        } break;
+        case 3: {
+            //learnWords(conf, file_content)
+        } break;
+        case 4: {
+            print_dir_cont_menu(conf);
+        } break;
+    }
+
+    if (file_content != null) {
+    } else {
+        printf("Cant open file with name %s\n", conf->current_file);
+        print_dir_cont_menu(conf);
+    }
 
 }
 
+void print_dir(JSON_OBJ *dir_cont) {
+        printf("Dir content:\n");
+        for(i32 i = 0; i < dir_cont->table_len; i++) {
+            printf("%d - %s\n", i + 1, dir_cont->table[i]->value);
+        }
+}
+
+void select_file(Config_main *conf, JSON_OBJ *dir_cont) {
+    print_dir(dir_cont);
+    i32 user_select = 0;
+    printf("Enter number for select file, or -1 for back to previous menu\n");
+    fscanf(stdin, "%d", &user_select);
+    if (user_select > 0 && user_select <= dir_cont->table_len) {
+        conf->current_file = dir_cont->table[user_select - 1]->value;
+        print_file_cont_menu(conf);
+    }
+    if (user_select == -1) {
+        print_folder_menu(conf);
+    }
+
+}
 
 void print_dir_cont_menu(Config_main *conf) {   
     JSON_OBJ *dir_cont = jsonGetObjByKey(conf->config, conf->current_dir);
@@ -58,15 +119,36 @@ void print_dir_cont_menu(Config_main *conf) {
         Config_destroy(conf);
         exit(1);
     } else {
-        printf("Dir content:\n");
-        for(i32 i = 0; i < dir_cont->table_len; i++) {
-            printf("%d - %s\n", i + 1, dir_cont->table[i]->value);
+        printf( "1. Show dir content\n" 
+                "2. Add file to dir\n"
+                "3. Delete file from dir\n"
+                "4. Select the file\n"
+                "4. Back to dir menu\n");
+
+        i32 user_select = 0;
+        switch(user_select) {
+            case 1: {
+                print_dir(dir_cont);
+            } break;
+            case 2: {
+                //TODO addFileToDir(dir_cont)
+            } break;
+            case 3: {
+                        //TODO deleteFile
+            } break;
+            case 4: {
+                        select_file(conf, dir_cont);
+            } break;
+            case 5: {
+                print_folder_menu(conf);
+            }
         }
+
         printf("Enter number for select file, or -1 for back to previous menu\n");
-        int user_select = 0;
         fscanf(stdin, "%d", &user_select);
         if (user_select > 0 && user_select <= dir_cont->table_len) {
-
+            conf->current_file = dir_cont->table[user_select - 1]->value;
+            print_file_cont_menu(conf);
         }
         if (user_select == -1) {
             print_folder_menu(conf);
@@ -88,7 +170,7 @@ void print_folder_menu(Config_main *conf) {
         int user_chose = 0;
         fscanf(stdin, "%d", &user_chose);
         if (user_chose > 0 && user_chose <= user_folders->table_len) {
-            conf->current_dir = user_folders->table[user_chose]->key;
+            conf->current_dir = user_folders->table[user_chose - 1]->key;
         } else if (user_chose == -1) {
             main_menu(conf);
         } 
