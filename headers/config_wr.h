621424810d4e9d8ad0c52fa974d1f3fa680c5891
Maxim Hrunenko <maxhrunenko@gmail.com>
2024-11-02 11:52:51 +0200

    Change concept and api

--- headers/config_wr.h
+++ headers/config_wr.h
@@ -5,23 +5,55 @@
 #include <cstdext/jsonparser.h>
 #include "rem_error.h"
 
+typedef struct file_change{
+    str file_name;
+    str file_path;
+
+    i8 *file_cont;
+    struct file_chage *next;
+}file_change;
+
+
 typedef struct {
     str user_name;
     str path_to_work_dir;
 
     str current_dir;
     str current_file;
+    file_change *changed_files;
     JSON_OBJ *config;
 }Config_main;
 
-Config_main *Config_read_config();
-REM_CODE Config_add_dir(Config_main *config, str dir_name);
-REM_CODE Config_add_file(Config_main *config, str file_name, str dir_index);
-REM_CODE Config_write_config(Config_main *config);
-void Config_destroy(Config_main *conf);
 
-void print_dir_cont_menu(Config_main *conf);
-void main_menu(Config_main *conf);
-void print_folder_menu(Config_main *conf);
+//MAIN MENU begin
+
+void remMeinMenu(Config_main *conf);
+void remSetPathToWorkDir(Config_main *conf);
+void remCreateFolder(Config_main *conf);
+void remExit(Config_main *conf);
+
+//MAIN MENU end
+
+//SELECT FOLDER begin
+void remSelectFolderMenu(Config_main *conf);
+
+//SELECT FOLDER end
+
+//FOLDER CONTENT begin
+void remFolderContentMenu(Config_main *conf, str folder_name);
+
+//FOLDER CONTENT end
+
+//FILE CONTENT bigin
+void remFileContentMenu(Config_main *conf, str file_name);
+//FILE CONTENT end
+
+
+//MEMORISING begin
+
+void remStartMemorise(Config_main *conf, str file_name);
+
+//MEMORISING end
+
 
 #endif //CONFIG_WR_H
