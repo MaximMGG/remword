ae8b548e29c3b814b4a500e7091315666e0c83c5
MaximMGG <maxhrunenko@gmail.com>
2025-06-06 17:47:27 +0000

    create read window but without tests

--- src/screen.zig
+++ src/screen.zig
@@ -14,6 +14,8 @@ pub const Screen = struct {
     y: c_int = 0,
     x: c_int = 0,
     cur_pos: c_int = 0,
+    read_buffer: [4096]u8 = .{0} ** 4096,
+    read_buffer_len: u32 = 0,
 
     pub fn init(self: *Screen) !*Screen {
         self.stdscr = c.initscr() orelse return error.InitScrError;
@@ -31,7 +33,26 @@ pub const Screen = struct {
     }
 
     pub fn readInput(self: *Screen) !void {
+        const y_pos = self.max_y - @as(c_int, 10);
+        const x_pos: i32 = @intFromFloat(@as(f32, @floatFromInt(self.max_y)) * 0.25);
+        const y_size = 3;
+        const x_size = @divTrunc(self.max_x, 2);
+        const read_win = c.newwin(y_pos, x_pos, y_size, x_size) orelse return error.NewwinError;
+        _ = c.box(read_win, 0, 0);
+        _ = c.wrefresh(read_win);
+        _ = c.keypad(read_win, true);
 
+        @memset(&self.read_buffer, 0);
+        self.read_buffer_len = 0;
+
+        c = c.wprintw(read_win, ">: ");
+        var ch: c_int = c.getch();
+        while(ch != c.KEY_ENTER) {
+            self.read_buffer[self.read_buffer_len] = @as(u8, @intCast(ch));
+            self.read_buffer_len += 1;
+            ch = c.getch();
+        }
+
+        _ = c.delwin(read_win);
     }
-
 };
